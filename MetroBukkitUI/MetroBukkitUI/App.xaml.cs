ac19f8b52bf4570848de7d3fe7283443b15c9466
Simon <Beatsleigher@gmail.com>
2017-08-15 17:18:15 +0200

    .

--- MetroBukkitUI/MetroBukkitUI/App.xaml.cs
+++ MetroBukkitUI/MetroBukkitUI/App.xaml.cs
@@ -24,7 +24,7 @@ namespace MetroBukkitUI {
 
             Settings.Default.SettingChanging += (s, evt) =>
                 Debug.WriteLine("Setting " + evt.SettingName + " changed from " + Settings.Default[evt.SettingName] + " to " + evt.NewValue);
-            Settings.Default.SettingsSaving += (s, evt) => 
+            Settings.Default.SettingsSaving += (s, evt) =>
                 Debug.WriteLine("Preferences have been saved.");
 
             // Check if computer has enough power to support a server
@@ -37,11 +37,11 @@ namespace MetroBukkitUI {
             var cpuMaxSpeed = 0;
             var ramCurrentAvailable = 0;
 
-            foreach (ManagementObject mObj in objSearcher.Get()) 
-                if (mObj["MaxClockSpeed"] != null) 
+            foreach (ManagementObject mObj in objSearcher.Get())
+                if (mObj["MaxClockSpeed"] != null)
                     try {
                         cpuMaxSpeed = int.Parse(mObj["MaxClockSpeed"].ToString());
-                    } catch (Exception ex) { cpuMaxSpeed = 0; }
+                    } catch (Exception) { cpuMaxSpeed = 0; }
 
             objSearcher = new ManagementObjectSearcher(@"root\CIMV2", "SELECT * FROM Win32_OperatingSystem");
             foreach (ManagementObject mObj in objSearcher.Get())
@@ -51,8 +51,8 @@ namespace MetroBukkitUI {
                     try { ramCurrentAvailable += ((int.Parse(mObj["FreeVirtualMemory"].ToString())) / 1024); } catch (Exception ex) { }
 
             //if (cpuMaxSpeed < 2000 || ramCurrentAvailable < 2000) {
-                new MinimumRequirementWindow(!(cpuMaxSpeed < 2000), !(ramCurrentAvailable < 2000), cpuMaxSpeed, ramCurrentAvailable).ShowDialog();
-                Application.Current.Shutdown();
+            new MinimumRequirementWindow(!(cpuMaxSpeed < 2000), !(ramCurrentAvailable < 2000), cpuMaxSpeed, ramCurrentAvailable).ShowDialog();
+            Application.Current.Shutdown();
             //}
 
         }
